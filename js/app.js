eebec2e988cb98bb4936b3f673c415e7eb051fc2
RaymartMl <raymart.b123@gmail.com>
2020-03-19 22:00:09 +0800

    Add About section and smooth scrolling

--- js/app.js
+++ js/app.js
@@ -2,3 +2,23 @@
 const year = document.querySelector('#year');
 const getYear = () => new Date().getFullYear();
 year.innerText = getYear();
+
+// Smooth scrolling
+$("nav ul li a[href^='#']").on('click', function(e) {
+  // prevent default anchor click behavior
+  e.preventDefault();
+  // store hash (id value)
+  const hash = this.hash;
+
+  // animate
+  $('html, body').animate(
+    {
+      scrollTop: $(hash).offset().top
+    },
+    700,
+    function() {
+      // when done, add hash to url
+      window.location.hash = hash;
+    }
+  );
+});
